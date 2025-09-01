10861be9811e0c30835dad5d774ea2b75daaaaaa
Venkata Abhi Ram Boddu (venboddu) <venboddu@cisco.com>
2025-09-01 10:56:31 +0530

    task Find_sum

---
+++ 01-09-25/find_sum.py
@@ -0,0 +1,12 @@
+#find_sum
+'''
+find_sum
+args: firstNum, SecondNumber
+returns sum of the numbers
+'''
+
+def find_sum(firstNum,SecondNum):
+    result = firstNum + SecondNum
+    return result
+
+print(find_sum(10,20))
